d66e577e43325953bec1344b4ca02debcd9359e1
Everton Cruz <cruz.everton@gmail.com>
2023-04-27 13:43:29 -0300

    RTL config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/next'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupeFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
 }
---
+++ src/components/index.tsx
@@ -0,0 +1,9 @@
+const Main: React.FC = () => {
+  return (
+    <main>
+      <h1>Test Library Next App</h1>
+    </main>
+  )
+}
+
+export default Main
